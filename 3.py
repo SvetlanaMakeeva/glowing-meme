3a268d973b385381d9f7b28032eb0fd97077f50b
SvetlanaMakeeva <74464836+SvetlanaMakeeva@users.noreply.github.com>
2020-12-18 15:58:01 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,17 +1,17 @@
 class X:
-        def __init__(self,a):
-              self.x=a
+    def __init__(self,a):
+        self.x=a
 
-        def check(self, a , b, c):
-              return a+b+c==self.x
+    def check(self, a , b, c):
+        return a+b+c==self.x
 
-if __name__=="__main__"
-       a = int(input())
-       b = int(input())
-       c = int(input())
+if __name__=="__main__":
+    a = int(input())
+    b = int(input())
+    c = int(input())
 
-       checker= X(180)
-       if checker.check(a,b,c):
-           print("да")
-       else:
-           print("нет")
+    checker= X(180)
+    if checker.check(a,b,c):
+        print("да")
+    else:
+        print("нет")
