ea56ec083e638f37ebcaf5c54cb9306349de2454
SvetlanaMakeeva <74464836+SvetlanaMakeeva@users.noreply.github.com>
2020-12-19 21:23:17 +0300

    Create .py

---
+++ .py
@@ -0,0 +1,11 @@
+import unittest
+from homework2 import ListContainer
+
+class TestQueueContainer(unittest.TestCase):
+
+    def _init_(self):
+        self.test_list = [16]
+
+    def testArea(self):
+        self.assertEqual(my_new_list.my_add(1, 15, "HelloWorld"), self.test_list.append("HelloWorld"))
+        self.assertEqual(my_new_list.my_cout(1, 15), self.test_list[15])
