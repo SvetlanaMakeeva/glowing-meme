f2a3da806e310e95f7e4d67b94f169c85b802beb
SvetlanaMakeeva <74464836+SvetlanaMakeeva@users.noreply.github.com>
2020-12-23 23:25:38 +0300

    Update .py

--- .py
+++ .py
@@ -1,28 +1,36 @@
-class ListContainer:
-
-    def __init__ (self):
-        self.my_list = [[None]*16, [None]*16]
-
-    def my_add(self, index, index2, element):
-        self.element = element
-        self.index = index
-        self.index2 = index2
-
-        self.my_list[self.index][self.index2] = self.element
-
-    def my_cout(self, index, index2):
-        self.index = index
-        self.index2 = index2
-        print (self.my_list [self.index][self.index2])
-
-    def showAll(self):
-        print (self.my_list)
-
-
-my_new_list = ListContainer()
-my_new_list.my_add(1, 5, 10)
-my_new_list.my_add(0, 5, "hello")
-my_new_list.my_add(1, 6, "world")
-my_new_list.showAll()
-my_new_list.my_cout(0,5)
-my_new_list.my_cout(1,6) 
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+        
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_node = None
+        
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("list is not empty") 
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item , " ")
+                n = n.nref        
+    def delete_at_end(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        n.pref.nref = None
