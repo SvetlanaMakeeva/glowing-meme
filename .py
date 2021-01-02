76ed4f7fc4b1a64d1597733fb980f78903fba906
SvetlanaMakeeva <74464836+SvetlanaMakeeva@users.noreply.github.com>
2021-01-02 20:42:48 +0300

    Update .py

--- .py
+++ .py
@@ -7,5 +7,5 @@ class TestQueueContainer(unittest.TestCase):
         self.test_node = None
 
     def testArea(self):
-        self.assertEqual(my_new_list.push(7)), (my_new_list.push(8)), (my_new_list.push(9)
+        self.assertEqual(my_new_list.push(7)), (my_new_list.push(8)), (my_new_list.push(9))
         self.assertEqual(my_new_list.delete_at_start())
